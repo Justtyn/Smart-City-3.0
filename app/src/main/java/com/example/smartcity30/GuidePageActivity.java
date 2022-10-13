0cc50ebaf67ef8db70fb689f437a1bc7091f5fef
Justtyn <1046220903@qq.com>
2022-10-13 20:51:47 +0800

    initialize 22/10/13

---
+++ app/src/main/java/com/example/smartcity30/GuidePageActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartcity30;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuidePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide_page);
+    }
+}
